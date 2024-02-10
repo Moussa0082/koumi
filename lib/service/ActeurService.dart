91544277b88704bc92acc49689467c2b21d7209f
ibrahim10sy <ibrahim10sy@gmail.com>
2024-02-10 21:32:52 +0000

    commit

---
+++ lib/service/ActeurService.dart
@@ -0,0 +1,190 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:koumi_app/models/Acteur.dart';
+import 'package:koumi_app/models/TypeActeur.dart';
+import 'package:path/path.dart';
+
+class ActeurService extends ChangeNotifier {
+  static const String baseUrl = 'http://10.0.2.2:9000/acteur';
+
+  List<Acteur> acteurList = [];
+
+  static Future<void> creerActeur({
+    required String nomActeur,
+    required String adresseActeur,
+    required String telephoneActeur,
+    required String whatsAppActeur,
+    String? latitude,
+    String? longitude,
+    required String niveau3PaysActeur,
+    required String localiteActeur,
+    required String emailActeur,
+    required String filiereActeur,
+    required List<TypeActeur> typeActeur,
+    File? photoSiegeActeur,
+    File? logoActeur,
+    required String passWord,
+    required String maillonActeur,
+  }) async {
+    try {
+      var requete = http.MultipartRequest('POST', Uri.parse('$baseUrl/create'));
+
+      if (photoSiegeActeur != null) {
+        requete.files.add(http.MultipartFile(
+            'image1',
+            photoSiegeActeur.readAsBytes().asStream(),
+            photoSiegeActeur.lengthSync(),
+            filename: basename(photoSiegeActeur.path)));
+      }
+
+      if (logoActeur != null) {
+        requete.files.add(http.MultipartFile('image2',
+            logoActeur.readAsBytes().asStream(), logoActeur.lengthSync(),
+            filename: basename(logoActeur.path)));
+      }
+
+      requete.fields['acteur'] = jsonEncode({
+        'nomActeur': nomActeur,
+        'adresseActeur': adresseActeur,
+        'telephoneActeur': telephoneActeur,
+        'whatsAppActeur': whatsAppActeur,
+        'latitude': latitude,
+        'longitude': longitude,
+        'niveau3PaysActeur': niveau3PaysActeur,
+        'localiteActeur': localiteActeur,
+        'emailActeur': emailActeur,
+        'filiereActeur': filiereActeur,
+        'typeActeur': typeActeur.toList(),
+        'photoSiegeActeur': "",
+        'logoActeur': "",
+        'passWord': passWord,
+        'maillonActeur': maillonActeur,
+      });
+
+      var response = await requete.send();
+      var responsed = await http.Response.fromStream(response);
+
+      if (response.statusCode == 200 || responsed.statusCode == 201) {
+        final donneesResponse = json.decode(responsed.body);
+        debugPrint('acteur service ${donneesResponse.toString()}');
+      } else {
+        throw Exception(
+            'Échec de la requête avec le code d\'état : ${responsed.statusCode}');
+      }
+    } catch (e) {
+        throw Exception(
+          'Une erreur s\'est produite lors de l\'ajout de acteur : $e');
+    }
+  }
+
+
+   static Future<void> updateActeur({
+    required String  idActeur,
+    required String nomActeur,
+    required String adresseActeur,
+    required String telephoneActeur,
+    required String whatsAppActeur,
+    String? latitude,
+    String? longitude,
+    required String niveau3PaysActeur,
+    required String localiteActeur,
+    required String emailActeur,
+    required String filiereActeur,
+    required List<TypeActeur> typeActeur,
+    File? photoSiegeActeur,
+    File? logoActeur,
+    required String passWord,
+    required String maillonActeur,
+  }) async {
+    try {
+      var requete = http.MultipartRequest('PUT', Uri.parse('$baseUrl/update/$idActeur'));
+
+      if (photoSiegeActeur != null) {
+        requete.files.add(http.MultipartFile(
+            'image1',
+            photoSiegeActeur.readAsBytes().asStream(),
+            photoSiegeActeur.lengthSync(),
+            filename: basename(photoSiegeActeur.path)));
+      }
+
+      if (logoActeur != null) {
+        requete.files.add(http.MultipartFile('image2',
+            logoActeur.readAsBytes().asStream(), logoActeur.lengthSync(),
+            filename: basename(logoActeur.path)));
+      }
+
+      requete.fields['acteur'] = jsonEncode({
+        'nomActeur': nomActeur,
+        'adresseActeur': adresseActeur,
+        'telephoneActeur': telephoneActeur,
+        'whatsAppActeur': whatsAppActeur,
+        'latitude': latitude,
+        'longitude': longitude,
+        'niveau3PaysActeur': niveau3PaysActeur,
+        'localiteActeur': localiteActeur,
+        'emailActeur': emailActeur,
+        'filiereActeur': filiereActeur,
+        'typeActeur': typeActeur.toList(),
+        'photoSiegeActeur': "",
+        'logoActeur': "",
+        'passWord': passWord,
+        'maillonActeur': maillonActeur,
+      });
+
+      var response = await requete.send();
+      var responsed = await http.Response.fromStream(response);
+
+      if (response.statusCode == 200 || responsed.statusCode == 201) {
+        final donneesResponse = json.decode(responsed.body);
+        debugPrint('acteur service ${donneesResponse.toString()}');
+      } else {
+        throw Exception(
+            'Échec de la requête avec le code d\'état : ${responsed.statusCode}');
+      }
+    } catch (e) {
+      throw Exception(
+          'Une erreur s\'est produite lors de la modification de acteur : $e');
+    }
+  }
+
+  Future<List<Acteur>> fetchActeur() async {
+    try {
+      final response = await http
+          .get(Uri.parse('$baseUrl/read'));
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        print("Fetching data");
+        List<dynamic> body = jsonDecode(utf8.decode(response.bodyBytes));
+        acteurList = body.map((e) => Acteur.fromMap(e)).toList();
+        debugPrint(acteurList.toString());
+        return acteurList;
+      } else {
+        acteurList = [];
+        print(
+            'Échec de la requête avec le code d\'état: ${response.statusCode}');
+        throw Exception(jsonDecode(utf8.decode(response.bodyBytes))["message"]);
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  Future deleteActeur(String idActeur) async {
+    final response =
+        await http.delete(Uri.parse('$baseUrl/delete/$idActeur'));
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      applyChange();
+    } else {
+      print('Échec de la requête avec le code d\'état: ${response.statusCode}');
+      throw Exception(jsonDecode(utf8.decode(response.bodyBytes))["message"]);
+    }
+  }
+
+  void applyChange() {
+    notifyListeners();
+  }
+}
