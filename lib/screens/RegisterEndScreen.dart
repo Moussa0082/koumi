500866023e5b46d73b5eeead70a543d2417d9663
MOUSSA BANE <bane8251@gmail.com>
2024-02-11 00:17:35 +0000

    ui register et login en cours

--- lib/screens/RegisterEndScreen.dart
+++ lib/screens/RegisterEndScreen.dart
@@ -8,8 +8,260 @@ class RegisterEndScreen extends StatefulWidget {
 }
 
 class _RegisterEndScreenState extends State<RegisterEndScreen> {
+
+
+
+  String maillon = "";
+  String password = "";
+  String confirmPassword = "";
+  String telephone = "";
+  String adresse = "";
+  String filiere = "";
+  String localisation = "";
+  bool _obscureText = true;
+
+   List<String> options = ["Option 1", "Option 2", "Option 3"];
+
+  // Valeur par défaut
+  String selectedOption = "Option 1";
+
+  TextEditingController localisationController = TextEditingController();
+  TextEditingController entrepriseController = TextEditingController();
+    TextEditingController passwordController = TextEditingController();
+  TextEditingController adresseController = TextEditingController();
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color(0xFFFFFFFF),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                    const SizedBox(height: 5,),
+                Container(
+                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    IconButton(
+                  onPressed: () {
+                    // Fonction de retour
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(
+                    Icons.arrow_back,
+                    color: Colors.black,
+                    size: 30,
+                  ),
+                  iconSize: 30,
+                  splashRadius: 20,
+                  padding: EdgeInsets.zero,
+                  constraints: BoxConstraints(minWidth: 40, minHeight: 40),
+                ),
+                  ],
+                 ),
+                ),
+              Center(child: Image.asset('assets/images/logo-pr.png', height: 300, width: 150,)),
+              //  Container(
+              //    height: 40,
+              //    decoration: BoxDecoration(
+              //      color: Color.fromARGB(255, 240, 178, 107),
+              //    ),
+              //    child: Center(
+              //      child: Row(
+              //        mainAxisAlignment: MainAxisAlignment.center,
+              //      children: [
+              //       const  Text("J'ai déjà un compte .", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),),
+              //        const SizedBox(width: 4,),
+              //        GestureDetector(child: const Text("Se connecter", style: TextStyle(color: Colors.blue, fontSize: 18, fontWeight: FontWeight.bold) , )),
+              //       ],
+              //    ),
+                 
+              //    ),
+                 
+              //  ),
+              Text("Completer votre profil", style: TextStyle(
+                fontSize: 22, 
+                fontWeight: FontWeight.bold,
+
+                ),
+                ),
+               Form(child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+               const SizedBox(height: 10,),
+                // debut fullname 
+                Padding(
+                  padding: const EdgeInsets.only(left:10.0),
+                  child: Text("Adresse  *", style: TextStyle(color: (Colors.black), fontSize: 18),),
+                ),
+                TextFormField(
+                    controller: adresseController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15)),
+                        labelText: "Votre adresse",
+                        hintText: "Entrez votre adresse de residence",
+                        
+                        ),
+                    keyboardType: TextInputType.text,
+                    validator: (val) {
+                      if (val == null || val.isEmpty) {
+                        return "Veillez entrez votre adresse de residence";
+                      } else {
+                        return null;
+                      }
+                    },
+                    onSaved: (val) => adresse = val!,
+                  ),
+                  // fin  adresse fullname
+  
+                      const SizedBox(height: 10,),
+                //Email debut 
+                Padding(
+                  padding: const EdgeInsets.only(left:10.0),
+                  child: Text("Maillon ", style: TextStyle(color: (Colors.black), fontSize: 18),),
+                ),
+                TextFormField(
+                    controller: entrepriseController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15)),
+                        labelText: "Maillon",
+                        hintText: "Entrez votre maillon",
+                       
+                        ),
+                    keyboardType: TextInputType.text,
+                    validator: (val) {
+                      if (val == null || val.isEmpty) {
+                        return "Veillez entrez votre maillon";
+                      } else {
+                        return null;
+                      }
+                    },
+                    onSaved: (val) => maillon = val!,
+                  ),
+                  // fin  adresse email
+                      const SizedBox(height: 10,),
+
+                      Padding(
+                  padding: const EdgeInsets.only(left:10.0),
+                  child: Text("Localisation", style: TextStyle(color: (Colors.black), fontSize: 18),),
+                ),
+                TextFormField(
+                    controller: localisationController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15)),
+                        labelText: "Localisation",
+                        hintText: "Votre localisation ",
+                        
+                        ),
+                    keyboardType: TextInputType.text,
+                    validator: (val) {
+                      if (val == null || val.isEmpty) {
+                        return "Veillez entrez votre localisation";
+                      } else {
+                        return null;
+                      }
+                    },
+                    onSaved: (val) => localisation = val!,
+                  ),
+                  // fin  téléphone 
+                  const SizedBox(height: 10,),
+
+                  //filier 
+                            Padding(
+                  padding: const EdgeInsets.only(left:10.0),
+                  child: Text("Filiere", style: TextStyle(color: (Colors.black), fontSize: 18),),
+                ),
+                TextFormField(
+                    controller: localisationController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15)),
+                        labelText: "Filière",
+                        hintText: "Votre filiere ",
+                        
+                        ),
+                    keyboardType: TextInputType.text,
+                    validator: (val) {
+                      if (val == null || val.isEmpty) {
+                        return "Veillez entrez votre filière";
+                      } else {
+                        return null;
+                      }
+                    },
+                    onSaved: (val) => filiere = val!,
+                  ),
+                  // fin  téléphone 
+                  const SizedBox(height: 10,),
+                  // end filiere acteur 
+      
+               const SizedBox(height: 10,),
+                       Padding(
+                  padding: const EdgeInsets.only(left:10.0),
+                  child: Text("Pays *", style: TextStyle(color: (Colors.black), fontSize: 18),),
+                ),
+
+                //  selcet le niveau 3 pays 
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: DropdownButton<String>(
+                menuMaxHeight: 90,
+              isExpanded: true,
+              value: selectedOption,
+              items: options.map((option) {
+                      return  DropdownMenuItem(
+                        value: option,
+                        child: Text(option),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        selectedOption = value!;
+                      });
+                },
+              
+               ),
+            ),
+                //end select  
+              const  SizedBox(height: 10,),
+
+                  Center(
+                    child: ElevatedButton(
+              onPressed: () {
+                // Handle button press action here
+              },
+              child: Text(
+                " Enregistrer ",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFFFF8A00), // Orange color code
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                minimumSize: Size(250, 40),
+              ),
+            ),
+       ),
+
+              ],
+               )),
+            
+              ],
+            ),
+            
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/RegisterScreen.dart
+++ lib/screens/RegisterScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:koumi_app/screens/RegisterEndScreen.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -9,16 +10,22 @@ class RegisterScreen extends StatefulWidget {
 
 class _RegisterScreenState extends State<RegisterScreen> {
 
-    String fullname = "";
+  String fullname = "";
   String password = "";
-  String firstName = "";
+  String telephone = "";
   String lastName = "";
   String email = "";
   bool _obscureText = true;
 
+   List<String> options = ["Option 1", "Option 2", "Option 3"];
+
+  // Valeur par défaut
+  String selectedOption = "Option 1";
+
   TextEditingController fullNameController = TextEditingController();
   TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
+  TextEditingController telephoneController = TextEditingController();
+  // TextEditingController Controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -54,10 +61,41 @@ class _RegisterScreenState extends State<RegisterScreen> {
                Form(child: Column(crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                const SizedBox(height: 10,),
-              // const Text("", style: TextStyle(fontSize: 20, fontWeight:FontWeight.bold , color: Color(0xFFF2B6706)),),
+                // debut fullname 
                 Padding(
                   padding: const EdgeInsets.only(left:10.0),
-                  child: Text("Email *", style: TextStyle(color: Color(0xFFF2B6706), fontSize: 18),),
+                  child: Text("Nom Complet *", style: TextStyle(color:  (Colors.black), fontSize: 18),),
+                ),
+                TextFormField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15)),
+                        labelText: "Nom Complet",
+                        hintText: "Entrez votre prenom et nom",
+                        // icon: const Icon(
+                        //   Icons.mail,
+                        //   color: Color(0xFFF2B6706),
+                        //   size: 20,
+                        // )
+                        ),
+                    keyboardType: TextInputType.text,
+                    validator: (val) {
+                      if (val == null || val.isEmpty) {
+                        return "Veillez entrez votre prenom et nom";
+                      } else {
+                        return null;
+                      }
+                    },
+                    onSaved: (val) => fullname = val!,
+                  ),
+                  // fin  adresse fullname
+  
+                      const SizedBox(height: 10,),
+                //Email debut 
+                Padding(
+                  padding: const EdgeInsets.only(left:10.0),
+                  child: Text("Email *", style: TextStyle(color:  (Colors.black), fontSize: 18),),
                 ),
                 TextFormField(
                     controller: emailController,
@@ -83,6 +121,91 @@ class _RegisterScreenState extends State<RegisterScreen> {
                     onSaved: (val) => email = val!,
                   ),
                   // fin  adresse email
+                      const SizedBox(height: 10,),
+
+                      Padding(
+                  padding: const EdgeInsets.only(left:10.0),
+                  child: Text("Téléphone *", style: TextStyle(color: (Colors.black), fontSize: 18),),
+                ),
+                TextFormField(
+                    controller: telephoneController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15)),
+                        labelText: "Téléphone",
+                        hintText: " (+223) XX XX XX XX ",
+                        // icon: const Icon(
+                        //   Icons.mail,
+                        //   color: Color(0xFFF2B6706),
+                        //   size: 20,
+                        // )
+                        ),
+                    keyboardType: TextInputType.phone,
+                    validator: (val) {
+                      if (val == null || val.isEmpty) {
+                        return "Veillez entrez votre numéro de téléphone";
+                      } else {
+                        return null;
+                      }
+                    },
+                    onSaved: (val) => telephone = val!,
+                  ),
+                  // fin  téléphone 
+              const SizedBox(height: 10,),
+                       Padding(
+                  padding: const EdgeInsets.only(left:10.0),
+                  child: Text("Type Acteur *", style: TextStyle(color: (Colors.black), fontSize: 18),),
+                ),
+
+                const SizedBox(height: 5),
+
+                //  selcet type acteur 
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: DropdownButton<String>(
+                menuMaxHeight: 90,
+              isExpanded: true,
+              value: selectedOption,
+              items: options.map((option) {
+                      return  DropdownMenuItem(
+                        value: option,
+                        child: Text(option),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        selectedOption = value!;
+                      });
+                },
+              
+               ),
+            ),
+                //end select type acteur 
+     const  SizedBox(height: 10,),
+
+                  Center(
+                    child: ElevatedButton(
+              onPressed: () {
+                // Handle button press action here
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const RegisterEndScreen() ));
+              },
+              child:  Text(
+                " Suivant ",
+                style:  TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFFFF8A00), // Orange color code
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                minimumSize: Size(250, 40),
+              ),
+            ),
+       ),
 
               ],
                )),
