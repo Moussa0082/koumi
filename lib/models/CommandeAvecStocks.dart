b4b92a0c77f818dc8d2fe8af3b4c01dbb13ad678
MOUSSA BANE <bane8251@gmail.com>
2024-05-02 10:19:52 +0000

    commande checked

--- lib/models/CommandeAvecStocks.dart
+++ lib/models/CommandeAvecStocks.dart
@@ -5,12 +5,14 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
+import 'package:koumi_app/models/Acteur.dart';
 import 'package:koumi_app/models/Commande.dart';
 import 'package:koumi_app/models/Intrant.dart';
 import 'package:koumi_app/models/Stock.dart';
 
 class CommandeAvecStocks {
   Commande? commande;
+  Acteur? acteur;
   List<Stock>? stocks;
   List<Intrant>? intrants;
   List<double>? quantitesDemandees;
@@ -19,6 +21,7 @@ class CommandeAvecStocks {
 
   CommandeAvecStocks({
      this.commande,
+     this.acteur,
      this.stocks,
      this.intrants,
      this.quantitesDemandees,
@@ -40,7 +43,8 @@ class CommandeAvecStocks {
 
   Map<String, dynamic> toJson() {
     return {
-      'commande': commande?.toJson(),
+      'commande': commande?.toMap(),
+      'acteur': acteur?.toMap(),
       'stocks': stocks?.map((stock) => stock.toMap()).toList(),
       'intrants': intrants?.map((intrant) => intrant.toMap()).toList(),
       'quantitesDemandees': quantitesDemandees,
@@ -48,9 +52,11 @@ class CommandeAvecStocks {
     };
   }
 
+  
   factory CommandeAvecStocks.fromMap(Map<String, dynamic> map) {
     return CommandeAvecStocks(
-      commande: Commande.fromMap(map['commande'] as Map<String,dynamic>),
+      commande: map['commande'] != null ? Commande.fromMap(map['commande'] as Map<String,dynamic>) : null,
+      acteur: map['acteur'] != null ? Acteur.fromMap(map['acteur'] as Map<String,dynamic>) : null,
       stocks: List<Stock>.from((map['stocks'] as List<int>).map<Stock>((x) => Stock.fromMap(x as Map<String,dynamic>),),),
       intrants: List<Intrant>.from((map['intrants'] as List<int>).map<Intrant>((x) => Intrant.fromMap(x as Map<String,dynamic>),),),
       quantitesDemandees: List<double>.from((map['quantitesDemandees'] as List<double>),),
@@ -59,10 +65,10 @@ class CommandeAvecStocks {
   }
 
 
-  // factory CommandeAvecStocks.fromJson(String source) => CommandeAvecStocks.fromMap(json.decode(source) as Map<String, dynamic>);
+  factory CommandeAvecStocks.fromJson(String source) => CommandeAvecStocks.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
-  String toString() => 'CommandeAvecStocks(commande: $commande, stocks: $stocks,intrants: $intrants ,quantitesDemandees: $quantitesDemandees, quantitesIntrants: $quantitesIntrants)';
+  String toString() => 'CommandeAvecStocks(commande: $commande,acteur:$acteur ,stocks: $stocks,intrants: $intrants ,quantitesDemandees: $quantitesDemandees, quantitesIntrants: $quantitesIntrants)';
 
   @override
   bool operator ==(covariant CommandeAvecStocks other) {
@@ -70,6 +76,7 @@ class CommandeAvecStocks {
   
     return 
       other.commande == commande &&
+      other.acteur == acteur &&
       listEquals(other.stocks, stocks) &&
       listEquals(other.intrants, intrants) &&
       listEquals(other.quantitesDemandees, quantitesDemandees)&&
@@ -77,5 +84,17 @@ class CommandeAvecStocks {
   }
 
   @override
-  int get hashCode => commande.hashCode ^ stocks.hashCode ^ intrants.hashCode ^ quantitesDemandees.hashCode ^ quantitesIntrants.hashCode;
+  int get hashCode => commande.hashCode ^ commande.hashCode ^ stocks.hashCode ^ intrants.hashCode ^ quantitesDemandees.hashCode ^ quantitesIntrants.hashCode;
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'commande': commande?.toMap(),
+      'acteur': acteur?.toMap(),
+      'stocks': stocks?.map((x) => x.toMap()).toList(),
+      'intrants': intrants?.map((x) => x.toMap()).toList(),
+      'quantitesDemandees': quantitesDemandees,
+      'quantitesIntrants': quantitesIntrants,
+    };
+  }
+
  }
